ee1445932514766bbb8f2e9bf034b77304925b27
NoNormalCreeper <2560359315@qq.com>
2022-02-01 16:54:39 +0800

    general beauty change

--- static/count.js
+++ static/count.js
@@ -42,7 +42,7 @@ function countdown(){
     var percent=(100-(diff_time0/(date_e-date_s)*100)).toFixed(5);
 
     // display
-    var output=("距离开学(<i>"+holiday_end+"</i>)还有<b>"+days+"</b>天<b>"+hours+"</b>小时<b>"+minutes+"</b>分钟<b>"+seconds+"</b>秒 (即<b>"+((diff_time.toFixed(3)).toLocaleString())+"</b>秒)")
+    var output=("<p>距离开学(<i>"+holiday_end+"</i>)还有</p><p style=\"font-size:2em;\"><b>"+days+"</b>天<b>"+hours+"</b>小时<b>"+minutes+"</b>分钟<b>"+seconds+"</b>秒 (即<b>"+((diff_time.toFixed(3)).toLocaleString())+"</b>秒)</p>")
     var timerDiv=document.getElementById("timer");
     timerDiv.innerHTML=output;
     // var percentDiv=document.getElementById("percent");
@@ -52,7 +52,6 @@ function countdown(){
     var barDiv=document.getElementById("bar1")
     barDiv.setAttribute("style",("width: "+percent+"%"));
     barDiv.innerHTML=("<b>"+percent+"%</b>");
-
 
 }
 
@@ -66,6 +65,7 @@ function submitEdit(){
         sessionStorage.setItem('holiday_end',get_res);
     // }
     // TODO: 判断输入是否合法
+    document.getElementById("toast-body-1").innerHTML=("成功修改开学时间为<b>"+date_e+"</b>！")
 }
 
 function resetHolidayEnd(){
