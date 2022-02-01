a73c932c689b673a79cf5be515bf6288ddb5a254
NoNormalCreeper <2560359315@qq.com>
2022-02-01 13:21:12 +0800

    allow temp edit of holiday_end

--- static/count.js
+++ static/count.js
@@ -1,6 +1,11 @@
 // config
 holiday_start="2022-1-14 17:00:00";    // 放假时间
-holiday_end="2022-2-13 16:30:00";     // 开学时间
+// holiday_end="2022-2-13 16:30:00";     // 开学时间
+if(window.sessionStorage){
+    holiday_end=sessionStorage.getItem('holiday_end');
+}else{
+    holiday_end="2022-2-13 16:30:00"
+}
 next_holiday="2022-7-10 00:30:00";    // 下一次放假时间估算值
 
 function format(num){   // 转换为2位数
@@ -25,7 +30,6 @@ function countdown(){
     // var ss=date_n.getSeconds();
     // var ms=date_n.getMilliseconds();
 
-
     // calc
     var diff_time0 = (date_e-date_n); // 时间差(ms)
     var diff_time = diff_time0/1000;      // 时间差(s)
@@ -35,14 +39,17 @@ function countdown(){
     var seconds = format(parseInt(diff_time%60));  // 以60秒为一整份 取余 剩下秒数
     var percent=(100-(diff_time0/(date_e-date_s)*100)).toFixed(5);
 
-
     // display
-    var output=("距离开学("+holiday_end+")还有"+days+"天"+hours+"小时"+minutes+"分钟"+seconds+"秒 (即"+((diff_time.toFixed(3)).toLocaleString())+"秒)")
+    var output=("距离开学(<i>"+holiday_end+"</i>)还有<b>"+days+"</b>天<b>"+hours+"</b>小时<b>"+minutes+"</b>分钟<b>"+seconds+"</b>秒 (即<b>"+((diff_time.toFixed(3)).toLocaleString())+"</b>秒)")
     var timerDiv=document.getElementById("timer");
-    timerDiv.innerText=output;
-
+    timerDiv.innerHTML=output;
     var percentDiv=document.getElementById("percent");
-    percentDiv.innerText=((percent.toLocaleString())+" %");
+    percentDiv.innerHTML=("<b>"+(percent.toLocaleString())+"</b> %");
+}
+
+function submitEdit(){
+    var get_res=document.getElementById("edit").value;
+    holiday_end=get_res;
 }
 
 function start(){
