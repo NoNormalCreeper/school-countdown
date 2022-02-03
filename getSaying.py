3918ef63174adb74ccf6ef9ec5078cfa0dc85af4
NoNormalCreeper <2560359315@qq.com>
2022-02-03 17:54:19 +0800

    getsaying: in case cant get saying

--- getSaying.py
+++ getSaying.py
@@ -5,8 +5,10 @@ import json
 url="https://v1.hitokoto.cn/?encode.json"
 
 def getSaying():
-    r=requests.get(url)
-    t=r.json()
-    content="「"+t["hitokoto"]+"」\t——"+t["from"] 
-
-    return content
+    try:
+        r=requests.get(url)
+        t=r.json()
+        content="「"+t["hitokoto"]+"」\t——"+t["from"] 
+        return content
+    except:
+        return "「作业做完了吗？」\t——李铭远"
