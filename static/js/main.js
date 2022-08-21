ae83baecfd81322992ebf8d07da9a9278bb446e0
NoNormalCreeper <2560359315@qq.com>
2022-08-21 20:15:23 +0800

    🎨💡tidy up codes

--- static/js/main.js
+++ static/js/main.js
@@ -69,34 +69,12 @@ function countdown() {
         "totalSec": ((diffTime.toFixed(3)).toLocaleString()),
         "holidayEndTime": `(${timeEnd.Format("yyyy/MM/dd HH:mm:ss")})`
     });
-    // var output = ("<p class=\"info-text\">距离开学(<i>" + holiday_end + isAdded + "</i>)还有</p><p style=\"font-size:1.6em; font-family: DINCond-Black;\">" + days + "天" + hours + "小时" + minutes + "分钟" + seconds + "秒</p><p style=\"font-size:1.2em; font-family: DINCond-Black;\">（即" + ((diff_time.toFixed(3)).toLocaleString()) + "秒）</p>")
-    // timer.rawHtml = output;
 
     // generate progress bar
     const barDiv = $("#bar");
     barDiv.attr("style", (`width: ${percent}%`));
     barDiv.attr("aria-valuenow", (percent));
     barDiv.text((`${percent}%`));
-
-    // generate emotion
-    // const emotionDiv = $("#emotion");
-    // var res = "../static/emotion-";
-    // var percent2 = 100 - percent; // 发现逻辑写反了之后的补救
-    // if (percent2 > 80) {
-    //     var tmp = "5";
-    // } else if (percent2 > 60) {
-    //     var tmp = "4";
-    // } else if (percent2 > 45) {
-    //     var tmp = "3";
-    // } else if (percent2 > 25) {
-    //     var tmp = "2";
-    // } else if (percent2 > 15) {
-    //     var tmp = "1";
-    // } else {
-    //     var tmp = "0";
-    // }
-    // res = res + tmp + ".png";
-    // emotionDiv.attr("src", res);
 }
 
 
@@ -111,9 +89,6 @@ submitButton.click(() => {
     editedEndDateObj.setHours(14);
     sessionStorage.setItem('holidayEnd', editedEndDateObj);
     holidayEnd = editedEndDateObj;
-    // }
-    // TODO: 判断输入是否合法
-    // $(".toast-body-1").html("成功修改开学时间为" + get_res + "！");
 })
 
 var resetButton = $("#reset-button");
@@ -134,23 +109,13 @@ async function fetchAsync(url) {
 }
 
 $(() => { // init
-    // $("#emotion").attr("style", "display:inline-block");
-    // $("#covid").attr("style", "display:inline-block");
-    resetButton.attr("value", defaultHolidayEnd);
-
-    // const url = "covisd_api";
-
-    // $.ajax(url, {
-    //     dataType: 'json',
-    //     success: (data, status) => {
-    //         confirmedCount.ccc = data.ccc;
-    //         updateTime.ut = "更新时间: "+ data.ut;
-    //     }
-    // });
     $.ajax("saying", {
         dataType: 'text',
         success: (data, status) => {
             $("#saying").text(data);
+        },
+        error: (xhr, status, error) => {
+            console.log(error);
         }
     })
 
