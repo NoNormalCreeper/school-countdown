1a9c0ca5db6d3fec87722253cace36bd095cc356
NoNormalCreeper <2560359315@qq.com>
2022-12-02 12:34:57 +0000

    🎨 format `main.py`

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-import os, time
+import os
+import time
 from flask import Flask, render_template, request, url_for, redirect, session
 from flask_bootstrap import Bootstrap
 import get_saying
@@ -7,34 +8,36 @@ import get_saying
 app = Flask(__name__)
 
 
-@app.route("/debug") 
+@app.route("/debug")
 def debug():
     return render_template("debug.html")
 
+
 @app.route("/", methods=["GET", "POST"])
 def index():
-    s=get_saying.get_saying()
-    c=get_saying.get_current_confirmed_count()
-    data={
-        "s":s,
+    s = get_saying.get_saying()
+    c = get_saying.get_current_confirmed_count()
+    data = {
+        "s": s,
     }
-    return render_template("index.html",data=data)
+    return render_template("index.html", data=data)
 
 
 @app.route("/covid_api", methods=["GET"])
 def covid_api():
-    c=get_saying.get_current_confirmed_count()
-    data={
-        "ccc":c['current_confirmed'],
+    c = get_saying.get_current_confirmed_count()
+    data = {
+        "ccc": c['current_confirmed'],
         "ut": c['update_time']
     }
     return data
-    
+
 
 @app.errorhandler(404)  # 404页面
 def not_found(e):
     print('404')
-    return render_template('404.html'), 404 
+    return render_template('404.html'), 404
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=5000,debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
